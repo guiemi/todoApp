a7aad7ac25b6c06dc81e74097e2b756433bf211f
guiemi <guiemi.teixeira@gmail.com>
2019-08-22 22:09:36 -0300

    Substitui função por classe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World!
     </div>
-  );
+    ) 
+  }
 }
 
 export default App;
