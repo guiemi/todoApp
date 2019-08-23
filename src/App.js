4d99776439733a99520ce948a771682aaa9bf0c5
guiemi <guiemi.teixeira@gmail.com>
2019-08-23 12:58:34 -0300

    fix typo

--- src/App.js
+++ src/App.js
@@ -9,6 +9,41 @@ class App extends Component {
       list:[]
     }
   }
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    })
+  }
+  addItem(){
+    //create item with unique id
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    }
+
+    //copy of current list of items
+    const list = [...this.state.list]
+
+    // add new item to list
+    list.push(newItem)
+
+    // update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    })
+  }
+  deleteItem(id){
+    // copy current list of items
+    const list = [...this.state.list]
+
+    //filter out item being deleted
+    const updatedList = list.filter(item => item.id !== id)
+
+    this.setState({list:updatedList})
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,6 +61,21 @@ class App extends Component {
           >
           Add
           </button>
+          <br/>
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button
+                  onClick={() => this.deleteItem(item.id)}
+                  >
+                  X
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
         </div>
     </div>
     ) 
